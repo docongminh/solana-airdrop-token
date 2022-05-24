652dc5bfebd89a8349792730f0beb6f376ec9fb2
minhdc <minhdc@fabbi.io>
2022-05-25 00:47:30 +0700

    UPDATE: redirect devnet

--- go/src/solana/constants.go
+++ go/src/solana/constants.go
@@ -1,6 +1,6 @@
 package solana
 
-const DEVNET = ""
+const DEVNET = "https://api.devnet.solana.com"
 const TESTNET = "https://api.testnet.solana.com"
 const MAINNET = "https://ssc-dao.genesysgo.net"
 const NETWORK = DEVNET
