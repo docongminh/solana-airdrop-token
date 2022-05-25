d45905b64c961cd2513ecfd995e995014ac64d5a
docongminh <cristiando0902@gmail.com>
2022-05-25 18:01:44 +0700

    UPDATE: get all signatures

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("comming soon !");
+use anyhow::Result;
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
+use std::{env, str::FromStr, time::Duration};
+use dotenv;
+mod signatures;
+use signatures::get_signatures;
+
+fn main() -> Result<()> {
+    // let args: Vec<String> = env::args().collect();
+    // let rpc = args[1].clone();
+    // let collection_id = Pubkey::from_str(&args[2].clone())?;
+    let rpc = "";
+    let wallet_address = Pubkey::from_str(&"ykkvsfEtAhc7faxK3uJTYMPBmtrisGkU9Kv4SnuxzB7")?;
+    let commitment = CommitmentConfig::from_str("confirmed")?;
+    let timeout = Duration::from_secs(300);
+    let client = RpcClient::new_with_timeout_and_commitment(rpc.clone(), timeout, commitment);
+
+    get_signatures(&client, &wallet_address);
+    Ok(())
 }
---
+++ rust/src/signatures.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use solana_client::rpc_client::{RpcClient};
+
+pub fn get_signatures(client: &RpcClient, wallet_address: &Pubkey) -> Result<()>{
+  let mut all_signatures = Vec::new();
+  let mut signatures = client.get_signatures_for_address(&wallet_address)?;
+  while signatures.len() > 0 {
+    let last_sig = Signature::from_str(&signatures[signatures.len()-1].signature)?;
+    all_signatures.append(&mut signatures)
+    let config = GetConfirmedSignaturesForAddress2Config {
+      before: Some(last_sig),
+      until: None,
+      limit: None,
+      commitment: Some(CommitmentConfig::confirmed()),
+    };
+    signatures = client.get_signatures_for_address_with_config(&wallet_address, config)?;
+    println!("Found {} signatures", all_signatures.len());
+    let mut file = std::fs::File::create(format!("{}_signatures.json", wallet_address))?;
+    serde_json::to_writer(&mut file, &all_signatures)?;
+    // return all_signatures;
+
+    Ok(())
+  }
+}
