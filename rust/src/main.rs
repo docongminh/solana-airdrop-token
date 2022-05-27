f09b053c02926e29e15d9edcd8d4b5121eeaeb89
docongminh <cristiando0902@gmail.com>
2022-05-27 12:19:59 +0700

    UPDATE: fixed get signatures by wallets

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,21 +1,27 @@
 use anyhow::Result;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
-use std::{env, str::FromStr, time::Duration};
-use dotenv;
-mod signatures;
+
+
+use dotenv::dotenv;
+use std::{
+    env,
+    str::FromStr,
+    time::Duration
+};
+mod signatures; 
 use signatures::get_signatures;
 
 fn main() -> Result<()> {
-    // let args: Vec<String> = env::args().collect();
-    // let rpc = args[1].clone();
-    // let collection_id = Pubkey::from_str(&args[2].clone())?;
-    let rpc = "";
-    let wallet_address = Pubkey::from_str(&"ykkvsfEtAhc7faxK3uJTYMPBmtrisGkU9Kv4SnuxzB7")?;
+    dotenv().ok();
+    let rpc = env::var("RPC").unwrap();
+    //
+    let wallet = env::var("WALLET_ADDRESS").unwrap();
+    let wallet_address = Pubkey::from_str(&wallet)?;
     let commitment = CommitmentConfig::from_str("confirmed")?;
     let timeout = Duration::from_secs(300);
     let client = RpcClient::new_with_timeout_and_commitment(rpc.clone(), timeout, commitment);
 
-    get_signatures(&client, &wallet_address);
+    get_signatures(&client, &wallet_address)?;
     Ok(())
 }
--- rust/src/signatures.rs
+++ rust/src/signatures.rs
@@ -1,12 +1,18 @@
-use anyhow::Result;
-use solana_client::rpc_client::{RpcClient};
+use anyhow::{Result, Ok};
+use serde_json;
+use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey, signature::Signature};
+use solana_client::rpc_client::{RpcClient, GetConfirmedSignaturesForAddress2Config};
+
+use std::{
+  str::FromStr
+};
 
 pub fn get_signatures(client: &RpcClient, wallet_address: &Pubkey) -> Result<()>{
   let mut all_signatures = Vec::new();
   let mut signatures = client.get_signatures_for_address(&wallet_address)?;
   while signatures.len() > 0 {
     let last_sig = Signature::from_str(&signatures[signatures.len()-1].signature)?;
-    all_signatures.append(&mut signatures)
+    all_signatures.append(&mut signatures);
     let config = GetConfirmedSignaturesForAddress2Config {
       before: Some(last_sig),
       until: None,
@@ -14,11 +20,9 @@ pub fn get_signatures(client: &RpcClient, wallet_address: &Pubkey) -> Result<()>
       commitment: Some(CommitmentConfig::confirmed()),
     };
     signatures = client.get_signatures_for_address_with_config(&wallet_address, config)?;
-    println!("Found {} signatures", all_signatures.len());
-    let mut file = std::fs::File::create(format!("{}_signatures.json", wallet_address))?;
-    serde_json::to_writer(&mut file, &all_signatures)?;
-    // return all_signatures;
-
-    Ok(())
-  }
+  };
+  println!("Found {} signatures", all_signatures.len());
+  let mut file = std::fs::File::create(format!("signatures_{}.json", all_signatures.len()))?;
+  serde_json::to_writer(&mut file, &all_signatures)?;
+  Ok(())
 }
